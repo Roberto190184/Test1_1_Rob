3398173296d083c23723dba382e6301c7c0b6cd4
Roberto190184 <robertodibari84@gmail.com>
2024-11-25 11:03:23 +0100

    mmm

--- app.py
+++ app.py
@@ -1,16 +1,38 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
-def somma(l1:float,l2:float):
-    a = l1+l2
-    return a
+
+
+
 
 def main():
-    st.text("Ciao questo front-end funziona")
-    num1=st.slider('Please inserisci lato1 rettangolo',0,100,2)
-    num2=st.slider('Please inserisci lato2 rettangolo',0,100,3)
-    r = somma(num1,num2)
-    st.write("la somma è",r)
+
+    pred = joblib.load("titanic_pip.pkl")
+    st.title("Predizione Sopravvivenza Titanic")
+    st.write("Questa app predice se un passeggero è sopravvissuto o meno basandosi sulle variabili inserite.")
+    with st.form("form_predizione"):
+        
+        Pclass = st.selectbox("Classe Passeggero (1=First, 2=Second, 3=Third)", [1, 2, 3])
+        Age = st.slider("Età (anni)", 0, 100, 30)
+        SibSp = st.number_input("Numero di Fratelli/Coniugi a bordo", min_value=0, max_value=10, step=1, value=0)
+        Parch = st.number_input("Numero di Genitori/Figli a bordo", min_value=0, max_value=10, step=1, value=0)
+        Fare = st.number_input("Tariffa pagata (£)", min_value=0.0, max_value=500.0, step=0.1, value=30.0)
+        Embarked_Q = st.selectbox("Imbarco da Queenstown?", [0, 1])
+        Embarked_S = st.selectbox("Imbarco da Southampton?", [0, 1])
+        Sex_male = st.selectbox("Sesso (0=Femmina, 1=Maschio)", [0, 1])
+
+submit = st.form_submit_button("Predici Sopravvivenza")
+
+if submit:
+        input_data = np.array([["Pclass", "Age", "SibSp", "Parch", "Fare", "Embarked_Q", "Embarked_S", "Sex_male"]])
+modello = carica_modello()
 
 
-if __name__ =="__main__":
-    main()
+
